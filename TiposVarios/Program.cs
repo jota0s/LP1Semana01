707ac286461586eed9621cbbe03b396d49436554
jota0s <joaosilvaa.work@gmail.com>
2023-03-02 20:21:42 +0000

    variáveis tipo char

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text;
 namespace TiposVarios
 {
     class Program
@@ -8,12 +8,19 @@ namespace TiposVarios
 
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             int p = 10; 
             int x = 20;
             System.Console.WriteLine(p);
             System.Console.WriteLine(x);
+
+            char Symbol = '\u2190';
+            Console.WriteLine(Symbol);
+            char Symbol2 = '\u2194';
+            Console.WriteLine(Symbol2);
+
         }
 
- 
+    
     }
 }
