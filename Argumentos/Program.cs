8685cf6d93ac3bbf0ff0e626971e05a94576d472
jota0s <joaosilvaa.work@gmail.com>
2023-03-02 19:13:23 +0000

    Argumentos

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -6,7 +6,10 @@ namespace Argumentos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for ( int i = 0; i <args.Length; i++)
+            
+                Console.WriteLine(args[i]);
+            
         }
     }
 }
