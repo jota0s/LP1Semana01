7237340efc3ae3bdbea4c13c420b996d36aed2db
jota0s <joaosilvaa.work@gmail.com>
2023-03-02 17:27:54 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP1Semana01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
