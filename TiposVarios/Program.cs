be1501b5ace28bbc3e0e8add4b8102a5924733da
jota0s <joaosilvaa.work@gmail.com>
2023-03-02 19:51:11 +0000

    variáveis inteiras

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int p = 10; 
+            int x = 20;
+            System.Console.WriteLine(p);
+            System.Console.WriteLine(x);
+        }
+
+ 
+    }
+}
