92b78cb5794e19647157c5a1dc633f8c559bcb3c
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-20 22:27:50 +0000

    Add Various Chars

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -21,6 +21,16 @@ namespace VariousTypes
             Console.WriteLine($"isto é um literal uint: {l}" );
             Console.WriteLine($"isto é um literal long: {k}" );
             Console.WriteLine($"isto é um literal ulong: {n}" );
+
+
+            char CheckMark = '\u2713';
+            Console.WriteLine($"isto é um CheckMark: {CheckMark}" );
+            char Star = '\u2605';
+            Console.WriteLine($"isto é uma Star: {Star}" );
+            char Hearth = '\u2764';
+            Console.WriteLine($"isto é um Hearth: {Hearth}" );
+            char Arrow = '\u2794';
+            Console.WriteLine($"isto é um Arrow: {Arrow}" );
         }
     }
 }
