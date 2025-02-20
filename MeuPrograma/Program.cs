a7cf114163cd71c8b6c970e8c6022f9ba31cd98c
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-20 20:03:03 +0000

    Write commentary

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Start of the class
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Class to print in console
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
