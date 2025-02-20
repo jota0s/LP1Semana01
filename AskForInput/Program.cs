82e2a4f39415055d0afad5443b865e2fc7198584
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-20 23:44:45 +0000

    Add sum to the numbers

---
+++ AskForInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("insert a number: ");
+            string n =Console.ReadLine();
+            int c = int.Parse( n );
+            Console.WriteLine("insert a number with decimal: ");
+            string d =Console.ReadLine();
+            float f = float .Parse( d);
+            Console.WriteLine($"this is the sum of the numbers: {c + f}");
+        }
+    }
+}
