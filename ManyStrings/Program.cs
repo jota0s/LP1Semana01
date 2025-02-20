431b398a92e41db8c88c2349cf16fd39ae345d80
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-20 23:01:22 +0000

    Create varius strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string star = "\u2605";
+            string heart = "\u279C";
+            string flag = "\u2690";  
+
+            Console.WriteLine("this is a star" + star);
+            Console.WriteLine("this is a star" + heart);
+            Console.WriteLine("this is a star" + flag);
+        }
+    }
+}
