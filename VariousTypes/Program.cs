975fce26c79373092f426709111094e6d2406a81
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-20 21:42:42 +0000

    Add Types of variable

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {   
+        
+
+        private static void Main(string[] args)
+        {
+         int numero = 8;
+         float pi = 3.1415f;
+         double x = 2.4567;
+         uint l = 4U;
+         long k = 3L;
+         ulong n = 6UL;  
+            
+            Console.WriteLine($"isto é um inteiro: {numero}" );
+            Console.WriteLine($"isto é um float: {pi}" );
+            Console.WriteLine($"isto é um numero real: {x}" );
+            Console.WriteLine($"isto é um literal uint: {l}" );
+            Console.WriteLine($"isto é um literal long: {k}" );
+            Console.WriteLine($"isto é um literal ulong: {n}" );
+        }
+    }
+}
