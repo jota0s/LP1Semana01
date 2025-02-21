a06d77636a9eabf0a23a6cf863232903614f80e6
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-21 14:38:18 +0000

    Add project Percentagem

---
+++ Percentagem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {   
+           
+            Console.WriteLine(" ");
+            string n1 =Console.ReadLine();
+            float a = float .Parse( n1);
+
+            Console.WriteLine(" ");
+            string n2 =Console.ReadLine();
+            float t = float .Parse( n2); 
+
+            Console.WriteLine(" ");
+            string n3 =Console.ReadLine();
+            float f = float .Parse( n3); 
+
+            Console.WriteLine(" ");
+            string n4 =Console.ReadLine();
+            float p = float .Parse( n4);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+            
+            
+
+           
+        }
+    }
+}
