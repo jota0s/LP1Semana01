29540e4113e0716ffc3916c6cb853499cf2fa766
Joao Silva <joaosilvaa.work@gmail.com>
2026-02-18 19:30:42 +0000

    add integer variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int test = 9;
+            float pi = 3.141f;
+            double t = 2.13456;
+            long a = 3L;
+
+
+            Console.WriteLine($"inteiro: {test}");
+            Console.WriteLine($"float: {pi}");
+            Console.WriteLine($"double: {t}");
+            Console.WriteLine($"long: {a}");
+            
+
+
+
+        }
+    }
+}
